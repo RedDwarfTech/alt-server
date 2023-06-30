a951afc952237ea8f66446e942f6b4be4de7f206
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-01 03:34:16 +0800

    refactor: simplicity the main function

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-use actix_web::{ web, App, HttpServer};
-use controller::app::alt_app_controller::{hello, echo, manual_hello};
-use sched::scrapy::check_tpl_task;
-
-pub mod sched;
-pub mod controller;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-
-    let task: tokio::task::JoinHandle<()> = tokio::spawn(check_tpl_task());
-    match task.await {
-        Ok(_) => println!("success!"),
-        Err(_) => println!("schedule task failed!"),
-    };
-
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            //.route("/path", web::get().to(get_app_by_tag))
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+fn main() {
+    println!("hello world!")
 }
