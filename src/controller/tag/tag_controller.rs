c8ed8f642dfac2a2492d700e48d735ab32ea43b8
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-01 21:02:17 +0800

    feat: add tag controller

--- src/controller/tag/tag_controller.rs
+++ src/controller/tag/tag_controller.rs
@@ -1,12 +1,12 @@
 use actix_web::{ HttpResponse, Responder, web};
 
-pub async fn get_apps_by_tag() -> impl Responder {
+pub async fn get_tags() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/apps")
-            .route("/{tag}", web::get().to(get_apps_by_tag))
+        web::scope("/tag")
+            .route("/list", web::get().to(get_tags))
     );
 }
