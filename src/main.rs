43bfeee3af46a18d5ff901a0fed82d05b209bd03
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-02 01:34:24 +0800

    refactor: tweak

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,6 @@
 #[macro_use]
 extern crate diesel;
 
-use actix_web::{App, HttpServer};
-use controller::{
-    app::alt_app_controller::{self},
-    health::health_controller,
-    tag::tag_controller,
-};
-
-pub mod controller;
-pub mod model;
-pub mod service;
-pub mod common;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .configure(alt_app_controller::config)
-            .configure(tag_controller::config)
-            .configure(health_controller::config)
-    })
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+fn main(){
+    println!("hello world!")
 }
